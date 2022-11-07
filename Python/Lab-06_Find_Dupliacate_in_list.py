481129cd4d04128be66ad6419f48f43c0fd06e0b
Ashish Sinha <84274841+Ashish-Sinha-programmer-202070@users.noreply.github.com>
2022-11-07 22:31:30 +0530

    Duplicate_in_list

---
+++ Python/Lab-06_Find_Dupliacate_in_list.py
@@ -0,0 +1,19 @@
+# Write a function to find Duplicate values in the List.
+def duplicate(input_list):
+	new_dict, new_list = {}, []
+
+	for i in input_list:
+		if not i in new_dict:
+			new_dict[i] = 1
+		else:
+			new_dict[i] += 1
+
+	for key, values in new_dict.items():
+		if values > 1:
+			new_list.append(key)
+
+	return new_list
+
+if __name__ == '__main__':
+	input_list = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]    
+	print(duplicate(input_list))
