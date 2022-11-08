d2494e6efec36e25b48a2434b6d94e7d249f7a19
Ashish Sinha <84274841+Ashish-Sinha-programmer-202070@users.noreply.github.com>
2022-11-08 22:37:13 +0530

    Updated

--- Python/Lab-06_Find_Dupliacate_in_list.py
+++ Python/Lab-06_Find_Dupliacate_in_list.py
@@ -1,4 +1,4 @@
-# Write a function to find Duplicate values in the List.
+#Write a function to find Duplicate values in the List.
 def duplicate(input_list):
 	new_dict, new_list = {}, []
 
@@ -17,3 +17,19 @@ def duplicate(input_list):
 if __name__ == '__main__':
 	input_list = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]    
 	print(duplicate(input_list))
+
+#Python program to print duplicates from
+#a list of integers
+list = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+
+uniqueList = []
+duplicateList = []
+
+for i in list:
+	if i not in uniqueList:
+		uniqueList.append(i)
+	elif i not in duplicateList:
+		duplicateList.append(i)
+
+print(duplicateList)
+
